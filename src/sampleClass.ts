7288d4a180dc0ec1619a726bc44e43c8d41408fb
enginearn <enginyaaaaan@gmail.com>
2023-04-28 08:42:51 +0900

    First commit

---
+++ src/sampleClass.ts
@@ -0,0 +1,8 @@
+export class SampleClass {
+    public tasu(
+        a: number,
+        b: number
+    ): number {
+        return a + b;
+    }
+}
---
+++ src/sampleFunction.ts
@@ -0,0 +1,6 @@
+export function hiku(
+    a: number,
+    b: number
+): number {
+    return a - b
+}
